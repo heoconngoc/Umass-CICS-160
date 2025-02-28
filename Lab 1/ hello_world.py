c78216f1e7dc0ef95246d17b2840b56bd6b309e6
Quoc Dat Pham <quocdatphamm@gmail.com>
2025-02-28 16:34:37 -0500

    Add files via upload

---
+++ Lab 1/ hello_world.py
@@ -0,0 +1,27 @@
+# print("Hello World")
+
+# a = input("Enter 1st Person Info (Format: Name-Age):")
+# b = input("Enter 2nd Person Info (Format: Name-Age):")
+# aInfo = a.split("-")
+# bInfo = b.split("-")
+# if(int(aInfo[1])>int(bInfo[1])):
+#   print(f"{aInfo[0]} is {int(aInfo[1])-int(bInfo[1])} years older than {bInfo[0]}")
+# elif(int(aInfo[1])<int(bInfo[1])):
+#   print(f"{bInfo[0]} is {int(bInfo[1])-int(aInfo[1])} years older than {aInfo[0]}")
+# else:
+#   print("2 Person have the same age!")
+
+# sum = 0
+# num = int(input("Enter a number"))
+# while num>0:
+#   sum += num
+#   num = int(input("Enter a number"))
+# print(f"Sum before adding negative num: {sum}")
+# print(f"Sum after adding negative num: {sum + num}")
+
+# num = int(input("How many number you want to enter? "))
+# lst = []
+# for i in range(num):
+#   lst.append(int(input(f"Enter number {i+1}: ")))
+# lst.reverse()
+# print(lst)
